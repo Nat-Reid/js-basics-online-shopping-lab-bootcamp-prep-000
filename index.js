efccd74907d12718a1f97975186a68eec9899217
Nat Reid <nathanielreid@gmail.com>
2018-10-13 23:18:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,25 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+   var item = {itemName : item, itemPrice: Math.floor(Math.random()*100)};
+   cart.push(item);
+   if (cart.contains(item)){
+     return "${item.itemName} has been added to your cart.";
+   }
+   else{
+     return "Error in adding item to cart";
+   }
 }
 
 function viewCart() {
-  // write your code here
+  var message = "In your cart, you have ";
+  for (let i=0; i<cart.length-1; i++){
+    let item = cart[i];
+    message += "${item.itemName} at $${item.itemPrice}, ";
+  }
+  item = cart.slice(-1)[0];
+  message += "and ${item.itemName} at $${item.itemPrice}."
+  return message;
 }
 
 function total() {
